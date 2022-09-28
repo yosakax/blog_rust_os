db071c5df51e9d4ae27c05ed09099366355c0b49
yosakax <fallingfluit@gmail.com>
2022-09-29 00:04:48 +0900

    article: minimal-rust-kernel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // Rustの標準ライブラリをリンクしない
+#![no_main] // Rustのエントリポイントを無効にする
+
+use core::panic::PanicInfo;
+
+// パニック時にこの関数が呼ばれる
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Asumi Kana\nhoge";
+// OSのエントリポイントを独自の関数で上書き
+// Cの呼び出し規約を使用することをコンパイラに伝えるために
+// extern "C"として定義する
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
